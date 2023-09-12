8e876d1b48b1b9e09250ab3a1b6d40bb6974479a
Kirtee Prajapati <95649744+kirteeprajapati@users.noreply.github.com>
2023-09-12 13:24:58 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,6 +25,7 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/ccc.jpg";
 import tesla from "./company/tesla.png";
+import gravity from "./company/Gravity.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
